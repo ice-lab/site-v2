f019f62d6b1246c39dc10a88fe84d0cb38781a77
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2021-06-01 15:34:20 +0800

    feat: add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,4 +32,19 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ]
 };
