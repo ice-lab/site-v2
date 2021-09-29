a0c04131d8a3a6e0dcfa50406d197c58fce3e17c
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2021-09-29 10:06:03 +0800

    chore: add china mirror

--- config/footer.js
+++ config/footer.js
@@ -11,7 +11,11 @@ module.exports = {
         {
           label: '前端环境准备',
           to: '/docs/resource/front-env'
-        }
+        },
+        {
+          to: 'https://iceteam.gitee.io',
+          label: '国内镜像站点',
+        },
       ],
     },
     {
