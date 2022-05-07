c3ea9f2dadad9f96ef2132c1fccd0e23673aff2e
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2022-05-07 10:12:10 +0800

    fix: basepath

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,37 @@ try {
   const getDefCloudInfo = require('@ali/get-def-cloud-info');
   const { baseUrl } = getDefCloudInfo();
 
-  config.baseUrl = baseUrl;
+  config.ssrTemplate = `<!DOCTYPE html>
+  <html <%~ it.htmlAttributes %>>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="generator" content="Docusaurus v<%= it.version %>">
+      <% if (it.noIndex) { %>
+        <meta name="robots" content="noindex, nofollow" />
+      <% } %>
+      <%~ it.headTags %>
+      <% it.metaAttributes.forEach((metaAttribute) => { %>
+        <%~ metaAttribute %>
+      <% }); %>
+      <% it.stylesheets.forEach((stylesheet) => { %>
+        <link rel="stylesheet" href="${baseUrl}<%= stylesheet %>" />
+      <% }); %>
+      <% it.scripts.forEach((script) => { %>
+        <link rel="preload" href="${baseUrl}<%= script %>" as="script">
+      <% }); %>
+    </head>
+    <body <%~ it.bodyAttributes %>>
+      <%~ it.preBodyTags %>
+      <div id="__docusaurus">
+        <%~ it.appHtml %>
+      </div>
+      <% it.scripts.forEach((script) => { %>
+        <script src="${baseUrl}<%= script %>"></script>
+      <% }); %>
+      <%~ it.postBodyTags %>
+    </body>
+  </html>`;
   console.log('set def baseurl', baseUrl);
 } catch(err) {}
 
