daca62ef62682b379b2caa052461c8ec59d5863c
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-09-06 17:03:47 +0800

    feat: support adding baseUrl for internal site (#71)

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
-import styles from './button.module.css'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './button.module.css';
+import { join } from '../../utils/path';
 
 function Button({ primary = true, url, children }) {
+  const context = useDocusaurusContext();
+  const source = join(context.siteConfig.baseUrl, url);
   return (
-    <a href={url} className={clsx(styles.button, primary && styles.primary)}>
+    <a href={source} className={clsx(styles.button, primary && styles.primary)}>
       {children}
     </a>
   );
